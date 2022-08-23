cfe20d5ad577de072270e52229cd597f46b72513
NF-02 <NF-02@outlook.com>
2022-08-23 15:50:19 +0800

    此分支为人脸检测在上面一版，窗口大小：1024*768

--- src/com/ffait/reid/ManageFaceMainFrame.java
+++ src/com/ffait/reid/ManageFaceMainFrame.java
@@ -60,7 +60,7 @@ public class ManageFaceMainFrame {
 	static BufferedImage showImg;
 	public ManageFaceMainFrame() {
 		initialize();
-		
+
 	}
 	//完善版本1.0
 	private void initialize() {
@@ -75,70 +75,63 @@ public class ManageFaceMainFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
-		
-		
+
+
 		name = new JTextArea("姓名：");
 		name.setFont(new Font("monospaced", Font.PLAIN, 40) );
 		name.setEditable(false);//模仿JLabel 禁止编辑文字
 		name.setBackground(new Color(238,238,238));//设置背景色和 窗体的背景色一样
-		name.setBounds(20, 40, 600, 120);
-		
-		
-//		idnum = new JTextArea("用户名(身份证号后六位)：");
-//		idnum.setFont(new Font("monospaced", Font.PLAIN, 20) );
-//		idnum.setEditable(false);//模仿JLabel 禁止编辑文字
-//		idnum.setBackground(new Color(238,238,238));//设置背景色和 窗体的背景色一样
-//		idnum.setBounds(20, 50, 600, 40);
-		
-//		trainProject = new JTextArea("培训项目：");
-//		trainProject.setLineWrap(true);
-//		trainProject.setFont(new Font("monospaced", Font.PLAIN, 20) );
-//		trainProject.setEditable(false);					//模仿JLabel 禁止编辑文字
-//		trainProject.setBackground(new Color(238,238,238)); //设置背景色和 窗体的背景色一样
-//		trainProject.setLineWrap(true);        				//激活自动换行功能 
-//		trainProject.setWrapStyleWord(true);  				// 激活断行不断字功能
-//		trainProject.setBounds(20, 90, 630, 50);		
-		
-		cameralable = new JLabel("");
-		cameralable.setBounds(32, 768-540-30, 960, 540);
 
-		
+		cameralable = new JLabel("");
+
+
+
 		photolable = new JLabel();
-		photolable.setBounds(750+72, 10, 180, 180);
-		
-		
+
+
 		//提示信息栏
 		message = new JLabel("");
 		message.setFont(new Font("黑体",Font.PLAIN,20));
 		message.setForeground(Color.RED);
-		message.setBounds(20, 160, 630, 50);
-		
+
 		frame.getContentPane().add(name);
-		//frame.getContentPane().add(idnum);
-		//frame.getContentPane().add(trainProject);
-		frame.getContentPane().add(cameralable);		
+
+		frame.getContentPane().add(cameralable);
 		frame.getContentPane().add(photolable);
-		
+
 		frame.getContentPane().add(message);
 
-		
+
+		//所有组件的大小位置
+//		message.setBounds(20, 160, 630, 50);
+//		photolable.setBounds(750+72, 10, 180, 180);
+//		cameralable.setBounds(32, 768-540-30, 960, 540);
+//		name.setBounds(20, 40, 600, 120);
+
+		//所有组件的大小位置 人脸在上 photolable图片改为160*160
+		message.setBounds(20, 160+540-10, 630, 50);
+		photolable.setBounds(960+32-160, 10+540+20, 180-20, 180-20);
+		cameralable.setBounds(32, 198-198+20, 960, 540);
+		name.setBounds(20, 40+540, 600, 120);
+
+
 		//1024*960版本
 //		frame.setBounds(0,0, 1024, 960);
-//		
+//
 //		name.setFont(new Font("monospaced", Font.PLAIN, 60) );
 //		name.setBounds(20, 100, 600, 120);
-//		
+//
 //		message.setBounds(20, 250, 630, 50);
 //		photolable.setBounds(750, 30, 252, 252);
 //		cameralable.setBounds(32, 380, 960, 540);
 
-		
+
 		int x = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() - frame.getWidth()) / 2;
 		int y = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - frame.getHeight()) / 2;
 		frame.setLocation(x, 0);
-	
-			
-	}	
+
+
+	}
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -153,110 +146,110 @@ public class ManageFaceMainFrame {
 		long pretime=System.currentTimeMillis();
 //		VideoCapture camera = new VideoCapture(0);
 		camera = new VideoCapture(0);
-		
+
 		if (!camera.isOpened()) {
 			System.out.println("Camera Error");
 		} else {
 			Mat frame = new Mat();
 			while (flag == 0) {
 				camera.read(frame);
-                BufferedImage bi=fs.mat2BI(frame);
-                long currenttime=System.currentTimeMillis();
-                if(currenttime-pretime>10000) {
-                	new Thread(new Runnable() {
+				BufferedImage bi=fs.mat2BI(frame);
+				long currenttime=System.currentTimeMillis();
+				if(currenttime-pretime>10000) {
+					new Thread(new Runnable() {
 						@Override
 						public void run() {
 							//long a=System.currentTimeMillis();
 							if(true){
 								if(message.getText() == null || message.getText() == "")
 									message.setText("正在识别中");
-									String s = fs.judgeMember(bi);  
-										//返回值 !null：验证通过
-									
-									if(null==s) {
-										message.setText("没有检测到人脸");
-									}else if("null".equals(s)) {
-										message.setText("没有检测到人脸");
-									}else if("noFace".equals(s)) {
-										message.setText("没有检测到人脸");
-									}else if ("noUser".equals(s)) {
-										message.setText("用户未注册！");
+								String s = fs.judgeMember(bi);
+								//返回值 !null：验证通过
+
+								if(null==s) {
+									message.setText("没有检测到人脸");
+								}else if("null".equals(s)) {
+									message.setText("没有检测到人脸");
+								}else if("noFace".equals(s)) {
+									message.setText("没有检测到人脸");
+								}else if ("noUser".equals(s)) {
+									message.setText("用户未注册！");
+								}
+								else {//验证成功
+									System.out.println(s);
+
+									int f1 = s.indexOf('_');
+									int f2 = s.indexOf('_',f1+1);
+									int f3 = s.indexOf('_',f2+1);
+									int f4 = s.indexOf('_',f3+1);
+									int f5 = s.indexOf('_',f4+1);
+
+									String userId = s.substring(0, f1);
+									String userCode = s.substring(f1+1,f2);
+									String userName = s.substring(f2+1,f3);
+									String roleId = s.substring(f3+1,f4);
+									String photoUrl = s.substring(f4+1,f5);
+									String projects = s.substring(f5+1);
+									BufferedImage photo = DownloadFromUrl.downloadBufferedImageFromUrl(ParameterOperate.extract("mainService")+photoUrl, "jpg");
+
+									ImageIcon  i = new ImageIcon(photo);
+									i.setImage(i.getImage().getScaledInstance(160,160,Image.SCALE_DEFAULT));//设置缩放
+									photolable.setIcon(i);
+									name.setText("姓名："+ userName);
+									//idnum.setText("身份证后六位："+userId);
+									//trainProject.setText("培训项目：" + projects);
+									if("2".equals(roleId)) {
+										message.setText("设备将启动!");
+
+									}else {
+										message.setText("身份验证成功,你没有权限开启设备");
 									}
-									else {//验证成功
-										System.out.println(s);
-										
-										int f1 = s.indexOf('_');
-								        int f2 = s.indexOf('_',f1+1);
-								        int f3 = s.indexOf('_',f2+1);
-								        int f4 = s.indexOf('_',f3+1);
-								        int f5 = s.indexOf('_',f4+1);
-								        
-								        String userId = s.substring(0, f1);
-								        String userCode = s.substring(f1+1,f2);
-								        String userName = s.substring(f2+1,f3);
-								        String roleId = s.substring(f3+1,f4);
-								        String photoUrl = s.substring(f4+1,f5);
-								        String projects = s.substring(f5+1);
-								        BufferedImage photo = DownloadFromUrl.downloadBufferedImageFromUrl(ParameterOperate.extract("mainService")+photoUrl, "jpg");
-								        
-								        ImageIcon  i = new ImageIcon(photo);
-										i.setImage(i.getImage().getScaledInstance(180,180,Image.SCALE_DEFAULT));//设置缩放
-										photolable.setIcon(i);
-										name.setText("姓名："+ userName);
-										//idnum.setText("身份证后六位："+userId);
-										//trainProject.setText("培训项目：" + projects);
+									try {
+										//验证成功 五秒后清除信息
+										Thread.sleep(5000);
 										if("2".equals(roleId)) {
-											 message.setText("设备将启动!");
-											
-										}else {
-											 message.setText("身份验证成功,你没有权限开启设备");
-										}
-										try {
-											//验证成功 五秒后清除信息
-											Thread.sleep(5000);
-											if("2".equals(roleId)) {
-												openSerialPort(ParameterOperate.extract("commName"));
-												if(sendData()) {
-													 try {
-															String appname=  ParameterOperate.extract("appname");
-															Runtime.getRuntime().exec(appname);
-														} catch (IOException e1) {
-															// TODO Auto-generated catch block
-															e1.printStackTrace();
-														}
-												    SerialPortManager.closePort(mSerialport);
-													System.exit(0);
+											openSerialPort(ParameterOperate.extract("commName"));
+											if(sendData()) {
+												try {
+													String appname=  ParameterOperate.extract("appname");
+													Runtime.getRuntime().exec(appname);
+												} catch (IOException e1) {
+													// TODO Auto-generated catch block
+													e1.printStackTrace();
 												}
-												
-											}else {
-												 message.setText("身份验证成功,你没有权限开启设备");
+												SerialPortManager.closePort(mSerialport);
+												System.exit(0);
 											}
-										} catch (InterruptedException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										}finally{
-											name.setText("姓名：");
-											//idnum.setText("身份证后六位：");
-											//trainProject.setText("培训项目：");
-											message.setText("正在识别中");
-											photolable.setIcon(null);
+
+										}else {
+											message.setText("身份验证成功,你没有权限开启设备");
 										}
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}finally{
+										name.setText("姓名：");
+										//idnum.setText("身份证后六位：");
+										//trainProject.setText("培训项目：");
+										message.setText("正在识别中");
+										photolable.setIcon(null);
 									}
-									
+								}
+
 							}
 						}
-                		
-                	}).start();
-                	pretime=currenttime;
-                }
-                showImg = BlackImg.deepCopy(bi);
-                
-                BlackImg.drawFace(showImg);
-                
-                cameralable.setIcon(new ImageIcon(showImg));
-                
-                BlackImg.drawFace(bi);
-                cameralable.setIcon(new ImageIcon(bi));
+
+					}).start();
+					pretime=currenttime;
+				}
+				showImg = BlackImg.deepCopy(bi);
+
+				BlackImg.drawFace(showImg);
+
+				cameralable.setIcon(new ImageIcon(showImg));
+
+				BlackImg.drawFace(bi);
+				cameralable.setIcon(new ImageIcon(bi));
 			}
 		}
 	}
